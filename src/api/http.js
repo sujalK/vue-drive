ef6b7af58186f9762d08ad118d78a21cd13bc9dd
Sujal Khatiwada <amarjack808@gmail.com>
2024-06-13 22:00:45 +0545

    Update http.js
    update baseURL on src/api/http.js

--- src/api/http.js
+++ src/api/http.js
@@ -6,7 +6,7 @@ const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const http = axios.create({
   // baseURL: 'https://localhost:3030',
-  baseURL: BASE_URL
+  baseURL: 'https://vue-drive-api.onrender.com'
 });
 
 /**
